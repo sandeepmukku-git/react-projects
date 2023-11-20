f800013e7b03b09c5d99cded4da4574d09adebc5
sandeepmukku <sandeep12akp@gmail.com>
2023-11-20 10:24:56 +0530

    Seven React Projects

---
+++ 07reactrouter/src/Components/GitHub/Github.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+//   const [data, setData] = useState({});
+
+//   useEffect(() => {
+//     fetch(`https://api.github.com/users/gaearon`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data);
+//         setData(data);
+//     } )
+//   }, [])
+  
+  const data = useLoaderData();
+  
+  
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} width={300} alt=''/>
+    </div>
+  )
+}
+
+export default Github
+
+
+export const githubInfoLoader = async () => {
+    const response =  await fetch(`https://api.github.com/users/gaearon`);
+    return response.json();
+}
